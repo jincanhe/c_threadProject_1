5dfd0606da82feabf39be283c089c7ec199e1ff4
598224311@qq.com <598224311@qq.com>
2023-10-18 21:20:56 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "thread"
+
+void test_fun(){ std::cout << "testing()";};
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::thread t(test_fun);
     return 0;
 }
+
